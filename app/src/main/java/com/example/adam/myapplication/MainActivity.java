35c84801ac77b4c14d479fa54f5873933a1bbf55
adamba12 <96adam@gmail.com>
2017-09-03 18:16:40 +0300

    s

--- app/src/main/java/com/example/adam/myapplication/MainActivity.java
+++ app/src/main/java/com/example/adam/myapplication/MainActivity.java
@@ -1,11 +1,24 @@
 package com.example.adam.myapplication;
 
+
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+
+
+
+
+import com.marothiatechs.fragments.Chats;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,18 +43,42 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        final Button buttonMainMenuToGroups = (Button) findViewById(R.id.mainMenuToGroupsActivity);
-        buttonMainMenuToGroups.setOnClickListener(new View.OnClickListener() {
+        final Button buttonMainMenuToChat = (Button) findViewById(R.id.mainMenuToChatActivity);
+        buttonMainMenuToChat.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), !!!!!!!!!!!!!!!!.class)); <-- adam, here put your new activity!
+                               startActivity(new Intent(getApplicationContext(), Chat.class));
+
             }
+final class SectionsPagerAdapter extends FragmentPagerAdapter {public SectionsPagerAdapter(FragmentManager fm) {
+
+
+            super(fm);
+            }@Override
+            public Fragment getItem(int position) {
+                if (position == 0) {
+                    return new Chats();
+                } else
+                    return Chat.PlaceholderFragment.newInstance(position + 1);
+            }@Override
+            public int getCount() {
+// Show 3 total pages.
+                return 3;
+            }@Override
+            public CharSequence getPageTitle(int position) {
+                Locale l = Locale.getDefault();
+                switch (position) {
+                    case 0:
+                        return getString(R.string.title_section1).toUpperCase(l);
+                    case 1:
+                        return getString(R.string.title_section2).toUpperCase(l);
+                    case 2:
+                        return getString(R.string.title_section3).toUpperCase(l);
+                }
+                return null;
+            }
+            }
+
+
         });
 
-
-
-    }
-
-    public void myFuckingButton() {
-
-    }
-}
+}}
